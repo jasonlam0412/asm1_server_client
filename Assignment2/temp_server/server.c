1de990282627c3b9479dcf99b2a1cd6439ebb2d4
Vacuum757 <58843137+Vacuum757@users.noreply.github.com>
2020-04-09 22:04:37 +0800

    Add files via upload

--- Assignment2/temp_server/server.c
+++ Assignment2/temp_server/server.c
@@ -10,171 +10,59 @@
 # include <sys/stat.h> 
 # include <netinet/in.h>
 # include <arpa/inet.h>
-# include <math.h>
-# include "myftp.h"
-# include "/home/timcheng/isa-l/include/erasure_code.h"
-
+#include "myftp.h"
 # define PORT 12345
-//# define PATH_MAX
-
+#define MAX 1000
 int list_compare(char *fileRequest);
 void send_list(int client_sd);
-void *client_action(int client_sd);
+void *client_action(void *client_sd);
 void send_toClient(int client_sd, char *filename);
 void recv_fromClient(int sd);
-void ISA_test();
-uint8_t* encode_data(int n, int k, Stripe *stripe, size_t block_size);
-pthread_t *thread_id;
 
+pthread_t *thread_id;
 int thread_num = 0;
 
 int main(int argc, char** argv){
-	
-	//assign port
-	int port = 12345;
-
-	//declare variables
-	int server_socket, client_socket, addr_size;
-	struct sockaddr_in server_addr;
-
-	server_socket = socket(AF_INET, SOCK_STREAM, 0);
-
-	long val = 1;
-	//socket error
-	if(setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(long)) == -1){
-		perror("setsockopt\n");
+	int len;
+	char inputBuffer[256] = {};
+	thread_id = (pthread_t *) malloc(sizeof(pthread_t));
+	int sd=socket(AF_INET,SOCK_STREAM,0);
+    long val = 1;
+	if(argc != 2){
+		printf("Fuck input port number\n");
 		exit(1);
 	}
-
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = INADDR_ANY;
-	server_addr.sin_port = htons(port);
-
-	printf("port = %d\n",port);
-
-	//binding
-	if(bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0){
-		printf("bind error: %s (Errno:%d)\n", strerror(errno), errno);
+	int port_num = atoi(argv[1]);
+    if(setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(long)) == -1){
+        perror("setsockopt");
+        exit(1);
+    }
+	int client_sd;
+	struct sockaddr_in server_addr;
+	//struct sockaddr_in client_addr;
+	memset(&server_addr,0,sizeof(server_addr));
+	server_addr.sin_family=AF_INET;
+	server_addr.sin_addr.s_addr=htonl(INADDR_ANY);
+	server_addr.sin_port=htons(port_num);
+	if(bind(sd,(struct sockaddr *) &server_addr,sizeof(server_addr))<0){
+		printf("bind error: %s (Errno:%d)\n",strerror(errno),errno);
 		exit(0);
 	}
-
-	//listen
-	if(listen(server_socket, 10) < 0){
-		printf("listen error: %s (Errno:%d)\n", strerror(errno), errno);
+	if(listen(sd,3)<0){
+		printf("listen error: %s (Errno:%d)\n",strerror(errno),errno);
 		exit(0);
 	}
-
-	int max_socket_so_far = 0, sd;
-	//create fd_set for select
-	fd_set current_sockets;
-	//empty the current_sockes
-	FD_ZERO(&current_sockets);
-	//set the server socket on current sockets
-	FD_SET(server_socket, &current_sockets);
-	max_socket_so_far = server_socket;
-
-	printf("FD_SETSIZE = %d\n",FD_SETSIZE);
-
-	printf("sever socket = %d\n", server_socket);
-
-	int isset;
-
-	for(int z = 0; z<11; z++){
-		isset = FD_ISSET(z, &current_sockets);
-		printf("%d:%d ",z,isset);
-	}
-	printf("\n");
 	
-	int max;
-
 	while(1){
-
-		fd_set ready_sockets = current_sockets;
-
-		struct timeval tv;
-		tv.tv_sec = 5;
-		tv.tv_usec = 0;
+		struct sockaddr_in client_addr;
 		
-		//int socket_count;
-		//select
-		printf("selecting\n");
-		if( (select(max_socket_so_far+1, &ready_sockets, NULL, NULL, &tv) )< 0 ){
-			printf("select error: %s (Errno:%d)\n", strerror(errno), errno);
-			exit(0);
-		}
-		printf("selected\n");
-		
-		printf("max_socket_so_far = %d\n",max_socket_so_far );
-		for(int i = 0; i <= max_socket_so_far; i++){
-
-			
-			if(FD_ISSET(i, &current_sockets)){
-				// start a new connection 
-				printf(" i = %d and server_socket = %d\n",i,server_socket);
-				if (i == server_socket){
-					struct sockaddr_in client_addr;
-					addr_size = sizeof(client_addr);
-					if( (client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &addr_size) < 0 )){
-						printf("accept error: %s (Errno:%d)\n", strerror(errno), errno);
-						exit(0);
-					}
-					printf("New connecion, socket fd is %d, ip is : %s, port : %d\n",client_socket, inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port) );
-					FD_SET(client_socket, &current_sockets);
-
-					for(int z = 0; z<10; z++){
-						isset = FD_ISSET(z, &current_sockets);
-						printf("%d:%d ",z,isset);
-					}
-					printf("\n");
-
-					if(client_socket > max_socket_so_far){
-						max_socket_so_far = client_socket;
-					}
-					printf("Your client_socket is %d\n",client_socket );
-					
-					max = -1;
-					FD_CLR(i, &current_sockets);
-					for(int z = 0; z<11; z++){
-						isset = FD_ISSET(z, &current_sockets);
-						printf("%d:%d ",z,isset);
-						if(isset > max){
-							max = z;
-						}
-					}
-					max_socket_so_far = max;
-
-					printf("\n");
-				
-					printf("max_socket_so_far = %d\n",max_socket_so_far );
-				}else{
-					// communicate with client
-					printf("I am here FUCK YOU\n");
-					client_action(i);
-					FD_CLR(i, &current_sockets);
-				}
-
-			}
-		}
-	}
-
-
-
-	
-
-	ISA_test();
-
-
-
-	
-
-		/*struct sockaddr_in client_addr;
-		
-		
+		int addr_len=sizeof(client_addr);
 		if((client_sd=accept(sd,(struct sockaddr *) &client_addr,&addr_len))<0){
 				printf("accept erro: %s (Errno:%d)\n",strerror(errno),errno);
 				exit(0);
 			}
 		pthread_create(&thread_id[thread_num], NULL, client_action, &client_sd);
+		printf("client_sd is %d\n",client_sd);
 		thread_num += 1;
 		pthread_t *newPointer = (pthread_t *) realloc(thread_id, sizeof(pthread_t) * (thread_num + 1));
 		if(newPointer != NULL){
@@ -182,10 +70,15 @@ int main(int argc, char** argv){
 		}
 		for(int i=0; i<thread_num; i++){
 			pthread_join(thread_id[i], NULL);
-		}*/
+		}
+
+        
 		
-	
-	
+		
+		
+	}
+	close(client_sd);
+	close(sd);
 	return 0;
 }
 
@@ -217,8 +110,7 @@ void send_toClient(int client_sd, char *file){
 	FILE_DATA->length = file_size;
 	FILE_DATA->length = FILE_DATA->length;
 	strcpy(FILE_DATA->payload,file);
-
-	if( (len=send(client_sd,FILE_DATA,sizeof(*FILE_DATA),0)) < 0 ){
+	if((len=send(client_sd,FILE_DATA,sizeof(*FILE_DATA),0))<0){
 		printf("Send Error: %s (Errno:%d)\n",strerror(errno),errno);
 		exit(0);
 	}
@@ -306,17 +198,13 @@ void send_list(int client_sd){
 		filename = (char *) realloc(filename, sizeof(filename) + sizeof(entry->d_name) + 1);
 		strcat(filename, entry->d_name);
 		strcat(filename, "\n");
-        printf("%s",filename);
-        printf("--------------------------\n");
 	}
-	printf("%s",filename);
 	int size;
 	size = strlen(filename);
-	//size = htonl(size);
+	size = htonl(size);
 	
 	REPLY = list_reply(filename, size);
-	//REPLY->length = htonl(REPLY->length);
-    //REPLY->type = htonl(REPLY->type);
+	REPLY->length = htonl(REPLY->length);
 	if((len=send(client_sd,REPLY,sizeof(*REPLY),0))<0){
 		printf("Send Error: %s (Errno:%d)\n",strerror(errno),errno);
 		exit(0);
@@ -325,11 +213,8 @@ void send_list(int client_sd){
 	free(REPLY);
 }
 
-void *client_action(int sd){
-	printf("I GOT IT\n");
-	//int client_sd = *((int *)sd);
-	int client_sd = sd;
-	printf("client_sd = %d\n", client_sd);
+void *client_action(void *sd){
+	int client_sd = *((int *)sd);
 	char filename[] = "fuck.txt";
 	int filelen = 9;
 	int len;
@@ -341,16 +226,10 @@ void *client_action(int sd){
 	//printf("Get:%s\n",inputBuffer);
 	
 	
-	if( (len=recv(client_sd,REQUEST,sizeof(*REQUEST),0)) < 0 ){
-		printf("Really an error\n");
+	if((len=recv(client_sd,REQUEST,sizeof(*REQUEST),0))<0){
 		printf("Send Error: %s (Errno:%d)\n",strerror(errno),errno);
 		exit(0);
 	}
-	print_debug(REQUEST);
-    //REQUEST->type = ntohl(REQUEST->type);
-    printf("16 = %d\n",ntohl(16));
-    print_debug(REQUEST);
-    
 	
 	if(REQUEST->type == 0xA1){
 		printf("stype LIST    : %x\n",REQUEST->type);
@@ -371,7 +250,6 @@ void *client_action(int sd){
 		}else{
 			printf("No such file\n");
 			REPLY = get_reply(0);
-            strcpy(REPLY->payload , " ");
 			if((len=send(client_sd,REPLY,sizeof(*REPLY),0))<0){
 				printf("Send Error: %s (Errno:%d)\n",strerror(errno),errno);
 				exit(0);
@@ -394,106 +272,9 @@ void *client_action(int sd){
 	}else{
 		printf("Fail\n");
 	}
-	
-	print_debug(REQUEST);
-    print_debug(REPLY);
 
+	print_debug(REQUEST);
 	free(REQUEST);
 	free(REPLY);
 	pthread_exit(NULL);
 }
-/*
-void ISA_test(){
-	FILE *fp;
-	char *line = NULL;
-    size_t length = 0;
-    ssize_t read;
-
-    fp = fopen("serverconfig.txt", "r");
-    if (fp == NULL){
-    	printf("File does not exist.\n");
-    	exit(0);
-    }
-
-    int n, k, ID, block_size, port_number;
-
-    int o = 0;
-    int temp_input[5];
-
-    while((read = getline(&line, &length, fp)) != -1){
-    	int temp = atoi(line);
-    	temp_input[o] = temp;
-    	o++;
-    }
-    n = temp_input[0];
-    k = temp_input[1];
-    ID = temp_input[2];
-    block_size = temp_input[3];
-    port_number = temp_input[4];
-    printf("n = %d\n",n);
-    printf("k = %d\n",k);
-    printf("ID = %d\n",ID);
-    printf("block_size = %d\n",block_size);
-    printf("port_number = %d\n",port_number);
-    fclose(fp);
-
-    char file_name[] = "data/test.txt";
-    fp = fopen(file_name, "r");
-    long int res = 0;
-    if(fp==NULL){
-    	printf("test.txt does not exist\n");
-    }else{
-    	fseek(fp, 0L, SEEK_END);
-    	res = ftell(fp);
-    	
-    	printf("size of the file is : %ld\n",res);
-    }
-    if(res>0){
- 		int no_of_stripe = (int)ceil((double)res/(block_size * k));
- 		printf("no_of_stripe = %d\n",no_of_stripe);
-    }
-    
-    Stripe *stripe = malloc(sizeof(Stripe));
-    stripe->encode_matrix = malloc(sizeof(unsigned char) * (n * k));
-    stripe->table = malloc(sizeof(unsigned char) * (32 * k * (n-k)));
-    stripe->data_block = malloc(sizeof(unsigned char *));
-    stripe->parity_block = malloc(sizeof(unsigned char *));
-
-    data
-    
-    fseek(fp, 0, SEEK_SET);
-    char buf[block_size];
-    int x = 0,numbytes;
-
-    while(!feof(fp)){
-		numbytes = fread(buf, sizeof(char), sizeof(buf), fp);
-		printf("fread %d bytes, ", numbytes);
-		stripe->blocks[x]->data = buf;
-		x++;
-	}
-
-    /*uint8_t *encode_matrix = malloc(sizeof(uint8_t) * (n * k));
-	uint8_t *errors_matrix = malloc(sizeof(uint8_t) * (n * k));
-	uint8_t *invert_matrix = malloc(sizeof(uint8_t) * (n * k));
-	uint8_t *table = malloc(sizeof(uint8_t) * (32 * k * (n-k)));
-
-	encode_data(n, k, stripe, block_size);
-
-}
-
-uint8_t* encode_data(int n, int k, Stripe *stripe, size_t block_size){
-	printf("GOOD 1\n");
-
-	gf_gen_rs_matrix(stripe->encode_matrix, n, k);
-
-	ec_init_tables(k, n - k, &stripe->encode_matrix[k*k], stripe->table);
-
-	unsigned char** blocks_data = malloc(sizeof(unsigned char **) * n);
-
-	for (int i = 0; i < n ; i++){
-		blocks_data[i] = stripe->blocks[i]->data;
-	}
-	ec_encode_data(block_size, k, n-k, stripe->table, blocks_data, &blocks_data[k]);
-
-	return stripe->encode_matrix;
-}*/
